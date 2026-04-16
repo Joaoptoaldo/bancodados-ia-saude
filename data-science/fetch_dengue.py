96a9c8922999dc12b4b30df247ba330d7d2fe42b
Laboratório <laboratorio@ACAD.UFN.EDU.BR>
2026-04-15 21:02:53 -0300

    altera indice

--- data-science/fetch_dengue.py
+++ data-science/fetch_dengue.py
@@ -1,22 +1,30 @@
 import pandas as pd
 
 URL = (
-    "https://info.dengue.mat.br/api/alertcity?geocode=4106902&disease=dengue&format=csv&ew_start=1&ew_end=50&ey_start=2017&ey_end=2017"  
+    "https://info.dengue.mat.br/api/alertcity"
+    "?geocode=4316907"  # Código IBGE correto
+    "&disease=dengue"
+    "&format=csv"
+    "&ew_start=1"
+    "&ew_end=52"
+    "&ey_start=2022"
+    "&ey_end=2023"
 )
 
 df = pd.read_csv(URL)
 
 df = df[
     [
-        "data_iniSE",
+       "data_iniSE",
         "casos",
-        "tempmin",
-        "umidmed",
+        # "temp_min", 
+        # "umid_med", 
         "Rt"
     ]
 ].dropna()
 
 df["media_movel"] = df["casos"].rolling(4).mean()
+
 df = df.dropna()
 
 df.to_json(
@@ -25,4 +33,4 @@ df.to_json(
     date_format="iso"
 )
 
-print("Dados salvos:", len(df))
+print("Dados epidemiológicos salvos:", len(df))
